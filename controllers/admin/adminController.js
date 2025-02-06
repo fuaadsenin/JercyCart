ab4db7b9022bf0d7c2412382611dc46dc2710b6b
fuaad senin <fuaads333@gmail.com>
2025-02-07 00:18:52 +0530

    it main

--- controllers/admin/adminController.js
+++ controllers/admin/adminController.js
@@ -1,7 +1,7 @@
 const User = require("../../models/userSchema");
 const Order = require("../../models/orderSchema");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const loadLogin = (req, res) => {
   if (req.session.admin) {
