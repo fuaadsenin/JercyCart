295438f1f2483a696429fe0637bdc3ae3a243218
fuaad senin <fuaads333@gmail.com>
2025-02-06 23:13:32 +0530

    i changed

--- config/passport.js
+++ config/passport.js
@@ -1,47 +1,47 @@
-const passport=require("passport");
-const GoogleStrategy=require("passport-google-oauth20").Strategy
-const User=require("../models/userSchema")
-const env=require("dotenv").config()
+const env = require("dotenv").config();
+const passport = require("passport");
 
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/userSchema");
 
-
-
-
-passport.use(new GoogleStrategy({
-    clientID:process.env.GOOGLE_CLIENT_ID,
-    clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:"https://jercycart.shop/auth/google/callback"
-},
-async (accessToken,refreshToken,profile,done)=>{
-    try {
-    let user=await User.findOne({googleId:profile.id})
-    if(user){
-        return done(null,user)
-    }else{
-        user=new User({
-            name:profile.displayName,
-            email:profile.emails[0].value,
-            googleId:profile.id,
-        })
-        await user.save()
-        return done(null,user)
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "https://jercycart.shop/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+        if (user) {
+          return done(null, user);
+        } else {
+          user = new User({
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            googleId: profile.id,
+          });
+          await user.save();
+          return done(null, user);
+        }
+      } catch (error) {
+        return done(error, null);
+      }
     }
-    } catch (error) {
-        return done(error,null)
-    }
-}
-))
+  )
+);
 
-passport.serializeUser((user,done)=>{
-        done(null,user.id)
-})
-passport.deserializeUser((id,done)=>{
-    User.findById(id)
-    .then(user=>{
-        done(null,user)
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then((user) => {
+      done(null, user);
     })
-    .catch(err=>{
-        done(err,null)
-    })
-})
-module.exports=passport
+    .catch((err) => {
+      done(err, null);
+    });
+});
+module.exports = passport;
